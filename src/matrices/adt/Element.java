e2b82e3877ea64d9c4f63975cc34f1110b52bc0b
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-04-23 20:05:26 +0200

    add sparse matrix

---
+++ src/matrices/adt/Element.java
@@ -0,0 +1,12 @@
+package matrices.adt;
+
+    public class Element{
+        //position of element in matrix
+        int rowNum;
+        int columnNum;
+        int value;//value of the element
+
+        public void create(Sparse structure){
+
+        }
+    }
---
+++ src/matrices/adt/Sparse.java
@@ -0,0 +1,9 @@
+package matrices.adt;
+
+public class Sparse{
+    //dimension is row*column
+    int row;//number of rows
+    int column;
+    int numNonZero;//count of non-zero elements
+    Element El;
+}
