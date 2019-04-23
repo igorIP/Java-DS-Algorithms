4f49f3155fd7af71a73c5b57950642c577822e4d
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-04-23 12:59:24 +0200

    names fix

--- src/array/adt/ArrayADT.java
+++ src/array/adt/ArrayADT.java
@@ -15,7 +15,7 @@ public class ArrayADT {
     //Methods
 
     //Binary Search with Loop
-    public int BinarySearchLoop(int key) {
+    public int binarySearchLoop(int key) {
         int first = 0;
         int last = length - 1;
         int mid = length / 2;
@@ -41,24 +41,24 @@ public class ArrayADT {
     }
 
     //Binary Search with Recursion
-    public int BinarySearchRecursion(int key, int first, int last) {
+    public int binarySearchRecursion(int key, int first, int last) {
         if (first <= last) {
             int mid = (first + last) / 2;
             if (key == arr[mid]) return mid;
-            else if (key < arr[mid]) return BinarySearchRecursion(key, first, mid - 1);
-            else return BinarySearchRecursion(key, mid + 1, last);
+            else if (key < arr[mid]) return binarySearchRecursion(key, first, mid - 1);
+            else return binarySearchRecursion(key, mid + 1, last);
         }
         return -1;
     }
 
-    public void Display() {
+    public void display() {
         System.out.printf("\nElements are\n");
         for (int i = 0; i < length; i++) {
             System.out.printf("%d ", arr[i]);
         }
     }
 
-    public void Sort() {
+    public void sort() {
         int temp;
         for (int i = 0; i < length - 1; i++) {
             for (int j = i + 1; j < length; j++) {
@@ -75,36 +75,36 @@ public class ArrayADT {
         }
     }
 
-    public boolean IsSorted() {
+    public boolean isSorted() {
         for (int i = 0; i < length - 1; i++) {
             if (arr[i] > arr[i + 1]) return false;
         }
         return  true;
     }
 
-    public void AppendAdd(int value) {
+    public void appendAdd(int value) {
         if (length < size) {
             arr[length] = value;
             length++;
         }
     }
 
-    public int Search(int x) {
+    public int search(int x) {
         for (int i = 0; i < length; i++) {
             if (x == arr[i]) return i;
         }
         return -1;
     }
 
-    public int Get(int index) {
+    public int get(int index) {
         return arr[index];
     }
 
-    public void Set(int index, int value) {
+    public void set(int index, int value) {
         arr[index] = value;
     }
 
-    public int Delete(int index) {
+    public int delete(int index) {
         int temp = arr[index];
         if (index >= 0 && index < length) {
             for (int i = index; i < length - 1; i++) {
@@ -115,7 +115,7 @@ public class ArrayADT {
         return temp;
     }
 
-    public void Insert(int index, int value) {
+    public void insert(int index, int value) {
         if (index >= 0 && index <= length) {
             for (int i = length - 1; i >= index; i--) {
                 arr[i + 1] = arr[i];
@@ -125,7 +125,7 @@ public class ArrayADT {
         }
     }
 
-    public void Reverse() {
+    public void reverse() {
         int i = 0;
         int k = length - 1;
         int temp;
@@ -137,14 +137,14 @@ public class ArrayADT {
         //System.arraycopy(temp, 0, arr, 0, length);
     }
 
-    public void ShiftLeft() {
+    public void shiftLeft() {
         for (int i = 0; i < length - 1; i++) {
             arr[i] = arr[i + 1];
         }
         arr[length - 1] = 0;
     }
 
-    public void RotateLeft() {
+    public void rotateLeft() {
         int temp = arr[0];
         for (int i = 0; i < length - 1; i++) {
             arr[i] = arr[i + 1];
@@ -152,7 +152,7 @@ public class ArrayADT {
         arr[length - 1] = temp;
     }
 
-    public int Max() {
+    public int max() {
         int max = arr[0];
         for (int i = 0; i < length; i++) {
             if (arr[i] > max) max = arr[i];
@@ -160,7 +160,7 @@ public class ArrayADT {
         return max;
     }
 
-    public int Min() {
+    public int min() {
         int min = arr[0];
         for (int i = 0; i < length; i++) {
             if (arr[i] < min) min = arr[i];
