1146c499182d4740e1515bf4bd7f65db43b6cd15
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-04-30 02:08:11 +0200

    add sparse matrix methods: read, addition

--- src/matrices/adt/sparse/matrix/Element.java
+++ src/matrices/adt/sparse/matrix/Element.java
@@ -4,32 +4,28 @@ import java.util.Scanner;
 
 public class Element {
     //position of the element in matrix
-    int rowPosition;
-    int columnPosition;
-    int value;//value of the element
+    public int rowPosition;
+    public int columnPosition;
+    public int value;//value of the element
 
-    public void create(Sparse structure) {
+    public void create(Sparse sparse) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Dimensions: ");
-        structure.row = sc.nextInt();
-        structure.column = sc.nextInt();
+        sparse.rows = sc.nextInt();
+        sparse.columns = sc.nextInt();
         System.out.println("Enter number of non-zero elements: ");
-        structure.numNonZero = sc.nextInt();
-        structure.El = new Element[structure.numNonZero];
+        sparse.numNonZeroElements = sc.nextInt();
+        sparse.el = new Element[sparse.numNonZeroElements];
 
         System.out.println("Enter all non-zero elements");
-        for (int i = 0; i < structure.numNonZero; i++) {
-            structure.El[i].rowPosition = sc.nextInt();
-            structure.El[i].columnPosition = sc.nextInt();
-            structure.El[i].value = sc.nextInt();
+        for (int i = 0; i < sparse.numNonZeroElements; i++) {
+            sparse.el[i].rowPosition = sc.nextInt();
+            sparse.el[i].columnPosition = sc.nextInt();
+            sparse.el[i].value = sc.nextInt();
         }
     }
 
-    public void AddTwoSparseMatrices(Sparse structure1, Sparse structure2){
-
-    }
-
-    public void display(){
+    public void AddTwoSparseMatrices(Sparse sparse1, Sparse sparse2){
 
     }
 }
--- src/matrices/adt/sparse/matrix/Sparse.java
+++ src/matrices/adt/sparse/matrix/Sparse.java
@@ -1,9 +1,72 @@
 package matrices.adt.sparse.matrix;
 
+import java.util.Scanner;
+
 public class Sparse {
     //dimension is row*column
-    int row;//number of rows
-    int column;
-    int numNonZero;//count of non-zero elements
-    Element[] El;
+    private int rows;//number of rows
+    private int columns;
+    private int numNonZeroElements;//number of non-zero elements
+    Element[] el;//el is pointer to an Object of type Element[] "meaning" (array with objects of type Element)
+
+    public Sparse(int rows, int columns, int numNonZeroElements) {
+        this.rows = rows;
+        this.columns = columns;
+        this.numNonZeroElements = numNonZeroElements;
+        el = new Element[this.numNonZeroElements];
+    }
+
+    public void read() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the non-zero elements: " + "\n");
+        for (int i = 0; i < numNonZeroElements; i++) {
+            el[i].rowPosition = scan.nextInt();
+            el[i].columnPosition = scan.nextInt();
+            el[i].value = scan.nextInt();
+        }
+    }
+
+    public void display() {
+        int k = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (el[k].columnPosition == i && el[k].rowPosition == j) {
+                    System.out.printf(el[k++].value + " ");
+                } else {
+                    System.out.printf("0 ");
+                }
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public Sparse AdditionOperator(Sparse sparse) {
+        int i, j, k;
+        if (rows != sparse.rows || columns != sparse.columns) {
+            return null;
+        }
+        Sparse sum = new Sparse(rows, columns, numNonZeroElements + sparse.numNonZeroElements);
+        i = j = k = 0;
+        while (i < numNonZeroElements || j < sparse.numNonZeroElements) {
+            if (el[i].rowPosition < sparse.el[j].rowPosition) {
+                sum.el[k++] = el[i++];
+            } else if (el[i].rowPosition > sparse.el[j].rowPosition) {
+                sum.el[k++] = el[j++];
+            } else {
+                if (el[i].columnPosition < sparse.el[j].columnPosition) {
+                    sum.el[k++] = el[i++];
+                } else if (el[i].columnPosition > sparse.el[j].columnPosition) {
+                    sum.el[k++] = el[j++];
+                } else {
+                    sum.el[k] = el[i];
+                    sum.el[k++].value = el[i++].value + sparse.el[j++].value;
+                }
+            }
+        }
+        for (; i < numNonZeroElements; i++) sum.el[k++] = el[i];
+        for (; j < sparse.numNonZeroElements; j++) sum.el[k++] = sparse.el[j];
+        sum.numNonZeroElements = k;
+
+        return sum;
+    }
 }
