1403f9ac403598c9d50adf428c76403f9a671c84
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-05-10 06:06:19 +0200

    created package: doubly linked list

---
+++ src/doublyLinkedList/App.java
@@ -0,0 +1,6 @@
+package doublyLinkedList;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/doublyLinkedList/Node.java
@@ -0,0 +1,7 @@
+package doublyLinkedList;
+
+public class Node {
+    Node previous;
+    int data;
+    Node next;
+}
