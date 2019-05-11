7dd17f705e0b9bc84851fd6fd128240dcfdb47a1
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-05-11 06:06:30 +0200

    created doubly linked list methods: create, insert, display and length

--- src/doublyLinkedList/App.java
+++ src/doublyLinkedList/App.java
@@ -2,5 +2,17 @@ package doublyLinkedList;
 
 public class App {
     public static void main(String[] args) {
+        int[] arr = {11, 22, 33, 44};
+        DoublyLinkedListAdt myList = new DoublyLinkedListAdt();
+        myList.create(arr);
+        myList.display();
+        System.out.println(myList.length());
+        myList.insert(0, 1);
+        myList.display();
+        myList.insert(1, 2);
+        myList.display();
+        myList.insert(6, 6);
+        myList.display();
+
     }
 }
--- src/doublyLinkedList/DoublyLinkedListAdt.java
+++ src/doublyLinkedList/DoublyLinkedListAdt.java
@@ -1,6 +1,77 @@
 package doublyLinkedList;
 
 public class DoublyLinkedListAdt {
-    Node head;
-    Node last;
+    private Node first;
+    private Node last;
+
+    public void create(int[] arr) {
+        if (arr.length == 0) return;
+
+        first = new Node();
+        first.previous = null;
+        first.data = arr[0];
+        first.next = null;
+        last = first;
+        Node node = first;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node newNode = new Node();
+            node.next = newNode;
+            newNode.previous = node;
+            newNode.data = arr[i];
+            last = newNode;
+            node = newNode;
+        }
+    }
+
+    public void insert(int index, int value) {
+        if (index < 0 || index > this.length()) return;
+        Node node = first;
+        Node newNode = new Node();
+        int count = 0;
+
+        if (index == 0) {
+            newNode.data = value;
+            newNode.next = first;
+            newNode.previous = null;
+            first.previous = newNode;
+            first = newNode;
+        } else if (index == this.length() * 10) {
+            newNode.data = value;
+            newNode.previous = last;
+            last.next = newNode;
+            last = newNode;
+        } else {
+            for (int i = 1; i < index ; i++) {
+                node = node.next;
+            }
+            newNode.data = value;
+            newNode.next = node.next;
+            newNode.previous = node;
+            if (newNode.next != null) newNode.next.previous = newNode;
+            else last = newNode;
+            node.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node node = first;
+        System.out.println("==Start==");
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println("\n" + "==End==");
+    }
+
+    public int length() {
+        Node node = first;
+        int count = 0;
+
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        return count;
+    }
 }
