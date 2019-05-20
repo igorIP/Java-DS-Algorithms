a5b9c6c856fd2f3d3c035e235d8a4addd82ef5f2
Igor Pavlov <igor.pavlov.email@gmail.com>
2019-05-21 01:06:24 +0200

    added quick-sort, merge, merge-sort and count-sort methods

--- src/sorting/App.java
+++ src/sorting/App.java
@@ -57,12 +57,157 @@ public class App {
         return arr;
     }
 
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low, j = high;
+
+        do {
+            do {
+                i++;
+            } while (arr[i] <= pivot);
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i < j) swap(arr, i, j);
+        } while (i < j);
+
+        swap(arr, low, j);
+        return j;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        int j;
+        if (low < high) {
+            j = partition(arr, low, high);
+            quickSort(arr, low, j);
+            quickSort(arr, j + 1, high);
+        }
+    }
+
+
+    //merge two lists represented by two arrays, using axillary array
+    static int[] arrC = new int[20];
+
+    public static void merge(int[] arrA, int[] arrB, int m, int n) {
+        int i, j, k;
+        i = j = k = 0;
+
+        while (i < m && j < n) {
+            if (arrA[i] < arrB[j]) {
+                arrC[k++] = arrA[i++];
+            } else {
+                arrC[k++] = arrB[j++];
+            }
+        }
+
+        for (; i < m; i++) {
+            arrC[k++] = arrA[i];
+        }
+        for (; j < n; j++) {
+            arrC[k++] = arrB[j];
+        }
+    }
+
+    //merge two lists represented in one array using axillary array
+    public static void merge(int[] arrA, int low, int mid, int high) {
+        int i, j, k;
+        i = k = low;
+        j = mid + 1;
+        int[] arrB = new int[high + 1];
+
+        while (i <= mid && j <= high) {
+            if (arrA[i] < arrA[j]) {
+                arrB[k++] = arrA[i++];
+            } else {
+                arrB[k++] = arrA[j++];
+            }
+        }
+
+        for (; i <= mid; i++) {
+            arrB[k++] = arrA[i];
+        }
+        for (; j <= high; j++) {
+            arrB[k++] = arrA[j];
+        }
+
+        for (i = low; i <= high; i++) {
+            arrA[i] = arrB[i];
+        }
+    }
+
+
+    //merge-sort two list represented in one array, Iterative-2-way merge sort
+    public static void mergeSortIterative(int[] arr, int n) {
+        int p, i, low, mid;
+        int high = 0;
+        for (p = 2; p <= n; p = p * 2) {
+            for (i = 0; i + p - 1 < n; i = i + p) {
+                low = i;
+                high = i + p - 1;
+                mid = (low + high) / 2;
+                merge(arr, low, mid, high);
+            }
+        }
+        if (p / 2 < n) {
+            merge(arr, 0, p / 2, high - 1);
+        }
+    }
+
+    //2-way merge sort, Recursively
+    public static void mergeSortRecursion(int[] arr, int low, int high) {
+        int mid;
+        if (low < high) {
+            mid = (low + high) / 2;
+            mergeSortRecursion(arr, low, mid);
+            mergeSortRecursion(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    public static void countSort(int[] arr, int n) {
+        int max = 0;
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+
+        int[] arrB = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            arrB[arr[i]] += 1;
+        }
+
+        for (int i = 0; i <= max; i++) {
+            if (arrB[i] != 0) {
+                while (arrB[i] > 0) {
+                    arr[k++] = i;
+                    arrB[i]--;
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 
-        int[] arr = {8, 5, 7, 3, 2};
+        int[] arr = {2, 5, 7, 33, 122};
+        int[] arrB = {1, 55, 22, 44, 222};
         //System.out.println(Arrays.toString(bubbleSort(arr, arr.length)));
         //System.out.println(Arrays.toString(insertionSort(arr, arr.length)));
-        System.out.println(Arrays.toString(selectionSort(arr, arr.length)));
+        //System.out.println(Arrays.toString(selectionSort(arr, arr.length)));
+        //int[] arr = {8, 5, 7, 3, 2, Integer.MAX_VALUE};
+        //quickSort(arr, 0, 5);
+        //System.out.println(Arrays.toString(arr));
+        //merge(arr, arrB, 5, 5);
+        //System.out.println(Arrays.toString(arrC));
+        //merge(arrB,0,2,4);
+        //System.out.println(Arrays.toString(arrB));
+        //mergeSortIterative(arrB, 4);
+        //System.out.println(Arrays.toString(arrB));
+        //mergeSortRecursion(arrB, 0, 4);
+        //System.out.println(Arrays.toString(arrB));
+        countSort(arrB,4);
+        System.out.println(Arrays.toString(arrB));
 
     }
 
